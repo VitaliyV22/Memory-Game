2f11eeaf1ca7676be936c09b3bb1530cc3b5cadb
VitaliyV22 <vitanskyyv@gmail.com>
2023-11-19 15:52:15 -0600

    Completed logic of the game

--- script.js
+++ script.js
@@ -1,88 +1,59 @@
 const gameContainer = document.getElementById("game");
 
-const COLORS = [
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-  "red",
-  "blue",
-  "green",
-  "orange",
-  "purple",
-];
+const colors = ["red", "blue", "green", "orange", "purple", "gold"];
 
+const doubleList = [...colors, ...colors];
+
+const tileCount = doubleList.length;
+
+let revealedList = 0;
 let activeTile = null;
-let waitEndMove = false
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want ot research more
-function shuffle(array) {
-  let counter = array.length;
+let waitEndMove = false;
 
-  // While there are elements in the array
-  while (counter > 0) {
-    // Pick a random index
-    let index = Math.floor(Math.random() * counter);
+function createDivsForColors(color) {
+  const newDiv = document.createElement("div");
+  // adds the class of tile in css to the new div
+  newDiv.classList.add("tile");
+  //record data when clicked on
+  newDiv.setAttribute("data-color", color);
+  newDiv.setAttribute("data-revealed", "false");
 
-    // Decrease counter by 1
-    counter--;
+  newDiv.addEventListener("click", () => {
+    if (waitEndMove) {
+      return;
+    }
 
-    // And swap the last element with it
-    let temp = array[counter];
-    array[counter] = array[index];
-    array[index] = temp;
-  }
+    newDiv.style.backgroundColor = color;
 
-  return array;
+    if (!activeTile) {
+      activeTile = newDiv;
+
+      return;
+    }
+
+    //Waiting for second click.Active card must match second click
+    waitEndMove = true;
+
+    setTimeout(() => {
+      // resetting if clicked not matching card
+      newDiv.style.backgroundColor = null;
+      activeTile.style.backgroundColor = null;
+
+      waitEndMove = false
+      activeTile = null
+    }, 1000);
+
+  });
+
+  return newDiv;
 }
 
-let shuffledColors = shuffle(COLORS);
+for (let i = 0; i < tileCount; i++) {
+  const randomTile = Math.floor(Math.random() * doubleList.length);
+  const color = doubleList[randomTile];
+  const tile = createDivsForColors(color);
+  // gives only a max of 2 colors
+  doubleList.splice(randomTile, 1);
 
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
-function createDivsForColors(colorArray) {
-  for (let color of colorArray) {
-    // create a new div
-    let newDiv = document.createElement("div");
-
-    newDiv.addEventListener("click", () => {
-      if (waitEndMove) {
-        return
-      }
-
-      newDiv.style.backgroundColor = color;
-      if (!activeTile) {
-        activeTile = newDiv;
-        return
-      }
-      //Waiting for second click.Active card must match second click
-      waitEndMove =true
-      setTimeout(() => {
-        // resetting if clicked not matching card
-        newDiv.style.backgroundColor = null
-        activeTile.style.backgroundColor = null
-      }, 1000)
-    });
-
-    // give it a class attribute for the value we are looping over
-    newDiv.classList.add(color);
-
-    // call a function handleCardClick when a div is clicked on
-    newDiv.addEventListener("click", handleCardClick);
-
-    // append the div to the element with an id of game
-    gameContainer.append(newDiv);
-  }
+  gameContainer.appendChild(tile);
 }
-
-// TODO: Implement this function!
-function handleCardClick(event) {
-  // you can use event.target to see which element was clicked
-  console.log("you just clicked", event.target);
-}
-
-// when the DOM loads
-createDivsForColors(shuffledColors);
